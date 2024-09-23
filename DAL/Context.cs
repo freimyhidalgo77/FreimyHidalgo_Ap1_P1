9b4466a3550db400e18346dbd40cad690c5ea50c
fahh7 <fahh7@FREIMYHP>
2024-09-23 19:28:17 -0400

    Creando el contexto.

---
+++ DAL/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FreimyHidalgo_Ap1_P1.Models;
+
+
+namespace FreimyHidalgo_Ap1_P1.DAL
+{
+	public class Context : DbContext
+	{
+
+		public Context(DbContextOptions<Context> option) : base(option) 
+		{ 
+		}
+
+		public DbSet<Registro> Regitro { get; set; }
+	}
+}
+   
