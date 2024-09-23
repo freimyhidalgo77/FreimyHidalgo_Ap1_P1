d454ea16dfa83723b2b91f010628f79d48dd4664
fahh7 <fahh7@FREIMYHP>
2024-09-23 19:25:04 -0400

    Creando Modelo.

---
+++ Models/Registro.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreimyHidalgo_Ap1_P1.Models
+{
+	public class Registro
+	{
+		[Key]
+
+		public string name { get; set; }
+
+		public int id { get; set; }
+	}
+}
+   
