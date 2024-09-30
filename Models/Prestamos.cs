1c6c4f0f143e8971e9cf00caaa8efa7a1f615dc7
fahh7 <fahh7@FREIMYHP>
2024-09-30 18:32:13 -0400

    Creando el modelo Prestamos.

---
+++ Models/Prestamos.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreimyHidalgo_Ap1_P1.Models
+
+{
+	public class Prestamos
+	{
+
+		[Key]
+
+		public int PrestamoId { get; set; }
+
+		[Required(ErrorMessage="Campo concepto obligatorio")]
+
+		public string Concepto { get; set; }
+
+
+		[Required(ErrorMessage = "Campo deudor obligatorio")]
+
+		public string deudor { get; set; }
+
+
+		[Required(ErrorMessage = "Campo monto obligatorio")]
+
+		public decimal  monto { get; set; }
+
+
+		  
+
+	}
+}
