95c5b90b10f23368b9ef028706b7d27c009f6811
fahh7 <fahh7@FREIMYHP>
2024-10-12 21:01:00 -0400

    Deudores terminado.

---
+++ Models/Deudores.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreimyHidalgo_Ap1_P1.Models
+{
+	public class Deudores
+	{
+
+		[Key]
+
+		public int DeudorId { get; set; }
+
+
+		[Required(ErrorMessage = "El campo nombre es obligatorio")]
+		[RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El campo solo puede contener letras y espacios.")]
+		public string nombre { get; set; }
+
+	}
+}
+ 
