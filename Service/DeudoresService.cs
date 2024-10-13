fe9c2b3787d12655b763c925a9ef55099469b441
fahh7 <fahh7@FREIMYHP>
2024-10-12 21:13:47 -0400

    Deudor service terminado.

---
+++ Service/DeudoresService.cs
@@ -0,0 +1,28 @@
+﻿using FreimyHidalgo_Ap1_P1.DAL;
+using FreimyHidalgo_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FreimyHidalgo_Ap1_P1.Service
+{
+	public class DeudoresService
+	{
+
+
+		private readonly Context _context;
+
+
+		public DeudoresService(Context context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<Deudores>> ObtenerEntidadAsync()
+		{
+			return await _context.Deudores.ToListAsync();
+		}
+
+
+
+	}
+}
