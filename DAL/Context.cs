2dda659341970fd83d1bbfdcb6e8ea8e78bb59fc
fahh7 <fahh7@FREIMYHP>
2024-10-25 15:40:28 -0400

    Context terminado.

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -13,7 +13,6 @@ namespace FreimyHidalgo_Ap1_P1.DAL
 
 		public DbSet<Prestamos> Prestamos { get; set; }
 		public DbSet<Deudores> Deudores { get; set; }
-
 		public DbSet<Cobros> Cobros { get; set; }
 		public DbSet<CobroDetalle> CobroDetalles { get; set; }
 
@@ -25,9 +24,9 @@ namespace FreimyHidalgo_Ap1_P1.DAL
 				new Deudores(){DeudorId = 1, nombre = "Juan Perez"},
 				new Deudores(){DeudorId = 2, nombre = "Cristal Hernandez"},
                 new Deudores(){DeudorId = 3, nombre = "Maria Sanchez"},
-                new Deudores(){DeudorId = 4, nombre = "Hancock Smith"}
-
-			});
+                new Deudores(){DeudorId = 4, nombre = "Hancock Smith"},
+           
+            });
 
 		}
 	}
